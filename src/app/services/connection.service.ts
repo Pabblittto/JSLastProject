f677917694d70edd61d17e2dc3980b86ad14414c
Pabblo <pawel.mat17@op.pl>
2019-12-13 10:47:23 +0100

    base service

---
+++ src/app/services/connection.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConnectionService {// serwis do łączenia się z bazą poprzez Observable
+
+  constructor() { }
+
+  
+
+
+}
